9230581d37501efbb3da738ab4b22fb48492098b
anjaligitlab27 <anjalikumari270704@gmail.com>
2025-12-16 22:00:54 +0530

    [Anjali] : added program for Power Calculation

---
+++ gcr-codebase/programming-prerequisite/PowerCalculation.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class PowerCalculation{
+    public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    double base=sc.nextDouble();
+    double exponent=sc.nextDouble();
+
+    double Power= Math.pow(base, exponent);
+    System.out.println("Result is : " + Power);
+
+    sc.close();
+    }
+}
