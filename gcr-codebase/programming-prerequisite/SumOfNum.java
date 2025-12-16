9f4524a81e4b7337af32e398a23055d4c5653aba
anjaligitlab27 <anjalikumari270704@gmail.com>
2025-12-16 21:09:48 +0530

    [Anjali] : added program of Sum of Two numbers

---
+++ gcr-codebase/programming-prerequisite/SumOfNum.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class SumOfNum{
+    public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    System.out.println("Sum of two number is:" + (a+b));
+    }
+}
