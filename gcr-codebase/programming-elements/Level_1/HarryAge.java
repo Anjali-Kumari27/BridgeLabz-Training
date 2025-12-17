75b87f8429ce9341cc064b1f467ecd0450b54cdc
anjaligitlab27 <anjalikumari270704@gmail.com>
2025-12-17 16:03:27 +0530

    [Anjali] : added correct modified program for calculating Harry's age

--- gcr-codebase/programming-elements/Level_1/HarryAge.java
+++ gcr-codebase/programming-elements/Level_1/HarryAge.java
@@ -1,9 +1,17 @@
+//Created Class name HarryAge for calculating age of Harry 
 public class HarryAge{
      public static void main(String args[]){
+
+
+     //created birthYear variable to indicating harry's birth year  
      int birthYear =2000;
      int currentYear = 2024;
      
+     //created variable named age which is calculating age of harry 
      int Age = (currentYear - birthYear);
+
+
+     //displaying harry's age 
      System.out.println ("Harry's age in " + currentYear + " is " + Age);
      }
 }
