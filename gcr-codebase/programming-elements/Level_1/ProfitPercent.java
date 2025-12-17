72eb99c2570c7d3e8ea6ee3bffec39d35e48fbd0
anjaligitlab27 <anjalikumari270704@gmail.com>
2025-12-17 16:17:17 +0530

    [Anjali] : added correct modified program for calculating profit and profit percentage

--- gcr-codebase/programming-elements/Level_1/ProfitPercent.java
+++ gcr-codebase/programming-elements/Level_1/ProfitPercent.java
@@ -1,11 +1,25 @@
+//Created Class ProfitPercent to calculate profit and profit percentage
 public class ProfitPercent{
     public static void main(String args[]){
-    int costPrice = 129;
-    int sellingPrice = 191;
-    int profit = sellingPrice - costPrice;
-    double profitPercentage = (profit / costPrice) * 100;
 
-    System.out.println("The Cost Price is INR " + costPrice + " and Selling Price is INR " + sellingPrice + "\n" + "The Profit is INR " + profit + " and the Profit Percentage is " + profitPercentage );
+    
+
+    //created variable costPrice and assigned value to it
+    int COST_PRICE = 129;
+
+    //created variable sellingPrice and assigned value to it
+    int SELLING_PRICE = 191;
+
+    //calculating profit here
+    int profit = SELLING_PRICE - COST_PRICE;
+
+    //calculating profitPercentage
+    double profitPercentage = (profit / COST_PRICE) * 100;
+
+
+
+    //Displaying result here 
+    System.out.println("The Cost Price is INR " + COST_PRICE + " and Selling Price is INR " + SELLING_PRICE + "\n" + "The Profit is INR " + profit + " and the Profit Percentage is " + profitPercentage );
 
     }
 }
