3ec7bc02d64514c12cef10658ebe9ca660c81862
anjaligitlab27 <anjalikumari270704@gmail.com>
2025-12-17 15:20:17 +0530

    [Anjali] : added correct program for calculating Area of Circle

--- gcr-codebase/programming-prerequisite/AreaOfCircle.java
+++ gcr-codebase/programming-prerequisite/AreaOfCircle.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 public class AreaOfCircle{
     public static void main(String args[]){
     Scanner sc=new Scanner(System.in);
-    float rad=sc.nextFloat();
-    float area=3.14 * rad * rad;
+    double rad=sc.nextDouble();
+    double area=3.14 * rad * rad;
     System.out.println("Area of circle is:" + area);
     }
 }
