5190977f03d6312f16fb020be381a86d62a02224
anjaligitlab27 <anjalikumari270704@gmail.com>
2025-12-17 15:14:00 +0530

    [Anjali] : added correct program for calculating Harry's age

--- gcr-codebase/programming-elements/Level_1/HarryAge.java
+++ gcr-codebase/programming-elements/Level_1/HarryAge.java
@@ -1,10 +1,10 @@
-public class KilometersToMiles{
+public class HarryAge{
      public static void main(String args[]){
      int birthYear =2000;
      int currentYear = 2024;
      
-     int Age = (CurrentYear - BirthYear);
-     System.out.println ("Harry's age in " + CurrentYear + "is" + Age);
+     int Age = (currentYear - birthYear);
+     System.out.println ("Harry's age in " + currentYear + " is " + Age);
      }
 }
 
