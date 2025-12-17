4b21e8934f979a1c42996aafd01b4c632a82d361
anjaligitlab27 <anjalikumari270704@gmail.com>
2025-12-17 13:18:46 +0530

    [Anjali]: added initial commit for Level_1

---
+++ gcr-codebase/programming-elements/Level_1/HarryAge.java
@@ -0,0 +1,9 @@
+public class HarryAge{
+     public static void main(String args[]){
+     int BirthYear = 2000;
+     int CurrentYear = 2024;
+     
+     int Age = (CurrentYear - BirthYear);
+     System.ot.println ("Harry's age in " + CurrentYear + "is" + Age);
+
+}
