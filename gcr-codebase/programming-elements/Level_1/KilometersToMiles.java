fa2a40d402f97cc16397d1485337f350a01a274b
anjaligitlab27 <anjalikumari270704@gmail.com>
2025-12-17 14:05:54 +0530

    [Anjali] : added program for converting distance into miles

---
+++ gcr-codebase/programming-elements/Level_1/KilometersToMiles.java
@@ -0,0 +1,8 @@
+public class KilometersToMiles{
+     public static void main(String args[]){
+     double kilometers = 94;
+     
+     double Age = (CurrentYear - BirthYear);
+     System.out.println ("Harry's age in " + CurrentYear + "is" + Age);
+
+}
