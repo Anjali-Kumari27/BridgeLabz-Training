802f6fbf2f2103e2337864eef7a42221b3ac375b
anjaligitlab27 <anjalikumari270704@gmail.com>
2025-12-18 23:11:52 +0530

    [Anjali] : added program using while loop for checking number if it is fizz , buzz  or fizzbuzz

---
+++ gcr-codebase/control-flow/LEVEL_2/FizzBuzzByWhile.java
@@ -0,0 +1,48 @@
+//created FizzBuzz Class which print fizz if number is multiple of 3, buzz if number is multiple of 5 , fizzbBuzz if that number is multiple of both 3 and 5, and prints the number itself if they are not multiple of 3 and 5
+import java.util.Scanner;
+
+public class FizzBuzzByWhile{
+    public static void main(String args[]){
+    Scanner input = new Scanner(System.in);
+
+    
+    
+    //taking number as input from user
+    int number = input.nextInt();
+
+    //checking number is positive or not
+    if(number > 0){
+       
+       int i=1;   
+       while(i<=number){
+
+          if((i%3==0) && (i%5==0))
+            {
+             System.out.println("FizzBuzz");
+            }
+           
+          else if(i % 5 == 0)
+            {
+             System.out.println("Buzz");
+            }
+          
+          else if(i%3==0)
+            {
+             System.out.println("Fizz");
+            }
+
+          else {
+              System.out.println(i);
+             }
+         
+         //incrementing value
+         i++;
+
+       }
+    }
+
+
+    input.close();
+
+    }
+}
