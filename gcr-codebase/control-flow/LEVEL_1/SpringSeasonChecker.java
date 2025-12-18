bee12fd4796ebb8e8be557d33b6379272e4770c6
anjaligitlab27 <anjalikumari270704@gmail.com>
2025-12-18 20:33:10 +0530

    [Anjali] : added program that checks whether the day of a particular month is spring season or not

---
+++ gcr-codebase/control-flow/LEVEL_1/SpringSeasonChecker.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class SpringSeasonChecker{
+    public static void main(String args[]){
+    Scanner input = new Scanner(System.in);
+
+    
+    //taking input from the command line as month and day 
+    int month = Integer.parseInt(args[0]);
+    int day = Integer.parseInt(args[1]);
+
+    
+    //checking it is spring season or not as it is from March 20 to June 20
+    if((month ==3 && day>=20) || (month==4 && day>=20) || (month==5 && day>=20) || (month==6 && day>=20))
+      
+      {
+        System.out.println("It's a Spring Season");
+      }
+
+    else{
+          System.out.println("Not a Spring Season");
+        }
+
+
+
+    input.close();
+
+    }
+}
