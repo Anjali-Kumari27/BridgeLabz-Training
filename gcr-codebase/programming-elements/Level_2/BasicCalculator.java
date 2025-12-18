5600248c897d8ba785a338a5854a745c1bcbc2f6
anjaligitlab27 <anjalikumari270704@gmail.com>
2025-12-18 11:13:35 +0530

    [Anjali] : added program in level2 for BasicCalculator using basic arithmetic operations

---
+++ gcr-codebase/programming-elements/Level_2/BasicCalculator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+//created Class BasicCalculator for performing arithmetic operations on two inputs giving by user
+public class BasicCalculator{
+    public static void main(String args[]){
+    Scanner input = new Scanner(System.in);
+
+
+    float number1 = input.nextFloat();
+    float number2 = input.nextFloat();
+
+    //Applied Arithmetic operations
+    float addition = (number1 + number2) ;
+    float subtraction = (number1 - number2);
+    float multiplication = (number1 * number2);
+    float division = (number1 / number2);
+    
+
+
+    //printing the result
+    System.out.println("The addition, subtraction, multiplication and division value of 2 numbers " + number1 + " and " + number2 + " is " + addition + ", " +  subtraction + ", " + multiplication + ", and " + division);
+
+    input.close();
+
+    }
+}
