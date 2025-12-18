112889704e3274dfc463e0b85ca1fb4a97a305df
anjaligitlab27 <anjalikumari270704@gmail.com>
2025-12-18 20:00:51 +0530

    [Anjali] : added program for displaying sum of double data type using While loop until user enters 0

---
+++ gcr-codebase/control-flow/LEVEL_1/DoubleSum.java
@@ -0,0 +1,36 @@
+//Created DoubleSum Class print the sum of numbers
+import java.util.Scanner;
+
+public class DoubleSum{
+    public static void main(String args[]){
+
+    //created a Scanner Object
+    Scanner input = new Scanner(System.in);
+
+    //created variable total of double type for further sum of numbers
+    double total = 0.0;
+
+    //taking value from user
+    double value = input.nextDouble();
+
+
+    
+
+    //using while loop for checking whether the value is equal to 0 if not then add into total
+    while(value!=0.0){
+        
+        //adding entered value by user into total
+        total += value;
+        
+        //printing the message again for getting input from user until they enter 0
+        System.out.println("give input again : ");
+        value= input.nextDouble();
+        }
+  
+   System.out.println("total sum is " + total);
+    
+   
+    input.close();
+
+    }
+}
