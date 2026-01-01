0fc2abc2d412b8ffca51e0cc0b02e35e4e8a220c
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-01-01 14:58:54 +0530

    [Anjali]: Added modified Person (Parent class)

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hybridinheritance/restaurantmanagementsystem/Person.java
@@ -0,0 +1,18 @@
+package com.inheritance.hybridinheritance.restaurantmanagementsystem;
+
+public class Person {
+
+    protected String name;
+    protected int id;
+
+    // Constructor
+    public Person(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    void displayPersonInfo() {
+        System.out.println("Name : " + name);
+        System.out.println("ID   : " + id);
+    }
+}
