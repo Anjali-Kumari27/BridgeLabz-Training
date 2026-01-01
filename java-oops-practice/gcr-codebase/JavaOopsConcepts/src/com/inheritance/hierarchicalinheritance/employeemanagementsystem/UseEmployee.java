b3282cd83b2e67f98dd0f6a4ae9001c102257d45
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-01-01 15:21:51 +0530

    [Anjali]: Added UseEmployee class which includes main method

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchicalinheritance/employeemanagementsystem/UseEmployee.java
@@ -0,0 +1,20 @@
+package com.inheritance.hierarchicalinheritance.employeemanagementsystem;
+
+public class UseEmployee {
+	public static void main(String[] args) {
+
+        Employee e1 = new Manager("Anjali", 101, 80000, 10);
+        Employee e2 = new Developer("Rahul", 102, 60000, "Java");
+        Employee e3 = new Intern("Priya", 103, 15000, 6);
+
+        e1.displayDetails();
+        System.out.println();
+
+        e2.displayDetails();
+        System.out.println();
+
+        e3.displayDetails();
+    }
+
+
+}
