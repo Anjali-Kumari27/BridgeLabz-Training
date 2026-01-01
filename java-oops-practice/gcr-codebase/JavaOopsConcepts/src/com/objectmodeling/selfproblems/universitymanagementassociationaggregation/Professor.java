0530c0d88b2e0e962611ba9f5356235534bdc08a
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-01-01 14:28:18 +0530

    [Anjali]: Added Professor class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/selfproblems/universitymanagementassociationaggregation/Professor.java
@@ -0,0 +1,14 @@
+package com.objectmodeling.selfproblems.universitymanagementassociationaggregation;
+
+public class Professor {
+
+    private String professorName;
+
+    public Professor(String professorName) {
+        this.professorName = professorName;
+    }
+
+    public String getProfessorName() {
+        return professorName;
+    }
+}
