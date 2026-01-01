dc85f0058d1bc0abffba0eca2bc2effef0143789
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-01-01 13:30:34 +0530

    [Anjali] : Added Patient class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/selfproblems/hospitaldoctorpatientassociationcommunication/Patient.java
@@ -0,0 +1,14 @@
+package com.objectmodeling.selfproblems.hospitaldoctorpatientassociationcommunication;
+
+public class Patient {
+
+    private String patientName;
+
+    public Patient(String patientName) {
+        this.patientName = patientName;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+}
