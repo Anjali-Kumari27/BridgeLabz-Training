18a4efc9cd9d0b822c0eba0c226a35067d7097f5
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-01-01 13:16:48 +0530

    [Anjali]: Added Employee class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/assistedproblems/companydepartmentcomposition/Employee.java
@@ -0,0 +1,16 @@
+package com.objectmodeling.assistedproblems.companydepartmentcomposition;
+
+public class Employee {
+
+    private String employeeName;
+
+    // Constructor
+    public Employee(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    // Getter
+    public String getEmployeeName() {
+        return employeeName;
+    }
+}
