57cb726df63238c9ab805ca65542edf04ada03e7
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-01-01 15:05:18 +0530

    [Anjali] : Added ElectricVehicle (sub class)

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hybridinheritance/vehiclemanagementsystem/ElectricVehicle.java
@@ -0,0 +1,16 @@
+package com.inheritance.hybridinheritance.vehiclemanagementsystem;
+
+public class ElectricVehicle extends Vehicle {
+
+    private int batteryCapacity; // in kWh
+
+    public ElectricVehicle(int maxSpeed, String model, int batteryCapacity) {
+        super(maxSpeed, model);
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    void charge() {
+        System.out.println("Charging electric vehicle...");
+        System.out.println("Battery Capacity : " + batteryCapacity + " kWh");
+    }
+}
