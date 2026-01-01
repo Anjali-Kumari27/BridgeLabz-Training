7b3d8a6e5b01884c32335e714c38cdf730b3158f
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-01-01 15:08:21 +0530

    [Anjali] : Added UseVehicle class which includes main method

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hybridinheritance/vehiclemanagementsystem/UseVehicle.java
@@ -0,0 +1,18 @@
+package com.inheritance.hybridinheritance.vehiclemanagementsystem;
+
+public class UseVehicle {
+	public static void main(String[] args) {
+
+        ElectricVehicle ev = new ElectricVehicle(160, "Tesla Model 3", 75);
+        PetrolVehicle pv = new PetrolVehicle(180, "Honda City", 40);
+
+        System.out.println("==========Electric Vehicle==========");
+        ev.displayInfo();
+        ev.charge();
+
+        System.out.println("\n===========Petrol Vehicle===========");
+        pv.displayInfo();
+        pv.refuel();
+    }
+
+}
