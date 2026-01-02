2ef2102a4e47e37ffb490860c4693dfede502c51
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-01-02 23:58:55 +0530

    [Anjali] : Added Rentable innterface

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/vehiclerentalapplication/Rentable.java
@@ -0,0 +1,10 @@
+package com.vehiclerentalapplication;
+
+//Interface to define rental behavior
+public interface Rentable {
+	
+	 // Method to calculate rent based on days
+	 double calculateRent(int days);
+
+
+}
