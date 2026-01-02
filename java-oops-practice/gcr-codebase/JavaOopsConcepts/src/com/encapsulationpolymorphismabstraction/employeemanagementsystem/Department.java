5cbff31ae75b0e86de6b86044a9ececd48ac5576
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-01-02 11:28:24 +0530

    [Anjali]: Added Department interface

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationpolymorphismabstraction/employeemanagementsystem/Department.java
@@ -0,0 +1,9 @@
+package com.encapsulationpolymorphismabstraction.employeemanagementsystem;
+
+public interface Department {
+	
+	void assignDepartment(String departmentName);
+
+    String getDepartmentDetails();
+
+}
