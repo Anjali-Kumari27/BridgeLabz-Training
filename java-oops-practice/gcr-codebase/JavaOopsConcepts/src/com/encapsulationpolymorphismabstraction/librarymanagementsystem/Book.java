e534d8acf1a97740975fd5069de9e1d4f5482059
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-01-02 11:45:05 +0530

    [Anjali] : Added Book (Sub class)

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationpolymorphismabstraction/librarymanagementsystem/Book.java
@@ -0,0 +1,28 @@
+package com.encapsulationpolymorphismabstraction.librarymanagementsystem;
+
+public class Book extends LibraryItem implements Reservable {
+
+    public Book(int itemId, String title, String author) {
+        super(itemId, title, author);
+    }
+
+    @Override
+    public int getLoanDuration() {
+        return 14; // days
+    }
+
+    @Override
+    public void reserveItem(String borrowerName) {
+        if (isAvailable()) {
+            setBorrower(borrowerName);
+            System.out.println("Book reserved for " + borrowerName);
+        } else {
+            System.out.println("Book not available");
+        }
+    }
+
+    @Override
+    public boolean checkAvailability() {
+        return isAvailable();
+    }
+}
