bc5a4eeb8fc386c60a97cb69b543aa16b5c835a9
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-01-02 23:50:19 +0530

    [Anjali]: Controllable interface

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/smarthomeautomationsystem/Controllable.java
@@ -0,0 +1,12 @@
+package com.smarthomeautomationsystem;
+
+//Interface defining common control operations for devices
+public interface Controllable {
+	
+	// Method to turn ON the device
+    void turnOn();
+    
+    // Method to turn OFF the device
+    void turnOff();
+    
+}
