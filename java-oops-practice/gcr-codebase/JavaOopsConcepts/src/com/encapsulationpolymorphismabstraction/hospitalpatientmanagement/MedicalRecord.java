2946b5a9c6dfd4f09ca3c3732c7936fea8d1b7f0
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-01-02 11:38:27 +0530

    [Anjali]: Added MedicalRecord interface

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationpolymorphismabstraction/hospitalpatientmanagement/MedicalRecord.java
@@ -0,0 +1,9 @@
+package com.encapsulationpolymorphismabstraction.hospitalpatientmanagement;
+
+public interface MedicalRecord {
+	
+	void addRecord(String diagnosis);
+
+    void viewRecords();
+
+}
