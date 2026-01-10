c41e69767202668c849ca1ff0fd7bb41ad7375a1
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-01-10 20:42:58 +0530

    [Anjali] : Added modified NonVegItem file

--- java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/foodloop/NonVegItem.java
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/foodloop/NonVegItem.java
@@ -2,10 +2,14 @@ package com.foodloop;
 
 public class NonVegItem extends FoodItem {
 
+	//constructor
     public NonVegItem(String name, double price, int stock) {
+    	
+    	//super keyword to call parent class constructor
         super(name, "Non-Veg", price, stock);
     }
 
+    //overriden method which displays the details of non veg item 
     @Override
     public void displayDetails() {
         System.out.println(name + " | INR " + price + 
