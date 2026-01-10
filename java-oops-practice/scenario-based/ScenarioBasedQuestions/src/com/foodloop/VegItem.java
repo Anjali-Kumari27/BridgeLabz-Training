989abd8810a1688e760b0ccacaecd42ad16828a1
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-01-10 21:02:26 +0530

    [Anjali] : Added modified VegItem file

--- java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/foodloop/VegItem.java
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/foodloop/VegItem.java
@@ -2,10 +2,14 @@ package com.foodloop;
 
 public class VegItem extends FoodItem {
 
+	//constructor
     public VegItem(String name, double price, int stock) {
+    	
+    	//super keyword to call parent class constructor
         super(name, "Veg", price, stock);
     }
 
+    //overriden method which display the details of veg item
     @Override
     public void displayDetails() {
         System.out.println(name + " | INR " + price + 
