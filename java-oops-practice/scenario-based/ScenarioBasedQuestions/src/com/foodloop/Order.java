aae407142f7270a33c54f01233ece85e2d0cf6f6
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-01-10 20:43:31 +0530

    [Anjali] : Added modified Order file

--- java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/foodloop/Order.java
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/foodloop/Order.java
@@ -5,14 +5,17 @@ import java.util.List;
 
 public class Order implements IOrderable {
 
+	//list to store food items
     private List<FoodItem> items;
     private double total;
 
+    //constructor 
     public Order() {
         items = new ArrayList<>();
         total = 0;
     }
 
+    //method to add food item
     public void addItem(FoodItem item) {
         if (item.isAvailable()) {
             items.add(item);
@@ -28,19 +31,37 @@ public class Order implements IOrderable {
     // Polymorphism
     public double applyDiscount() {
         if (total >= 500)
-            return total * 0.9;   // 10% discount
+        	// 9% discount if total is greater than 500
+            return total * 0.9;   
         return total;
     }
 
     @Override
     public void placeOrder() {
         System.out.println("\nOrder Placed Successfully");
-        System.out.println("Total Amount: ₹" + total);
-        System.out.println("Final Amount: ₹" + applyDiscount());
+        System.out.println("Total Amount: INR " + total);
+        System.out.println("Final Amount: INR " + applyDiscount());
     }
 
+    //method to cancel the order
     @Override
     public void cancelOrder() {
-        System.out.println("Order Cancelled");
+
+    	//check if item is not selected then it shows
+        if (items.isEmpty()) {
+            System.out.println("No items to cancel. Order is empty.");
+            return;
+        }
+
+        // Restore stock if user cancels the order
+        for (FoodItem item : items) {
+            item.increaseStock();
+        }
+
+        items.clear();
+        total = 0;
+
+        System.out.println("Order Cancelled Successfully");
     }
+
 }
