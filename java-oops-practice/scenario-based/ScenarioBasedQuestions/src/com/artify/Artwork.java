97957d218f4f03349be07d49011764fc76d81bc1
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-01-10 16:13:49 +0530

    [Anjali]: Added modifile artwork file

--- java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/artify/Artwork.java
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/artify/Artwork.java
@@ -2,43 +2,46 @@ package com.artify;
 
 public abstract class Artwork implements IPurchasable {
 
-    protected String title;
-    protected String artist;
-    protected double price;
-    protected String licenseType;
+	// instance variables
+	protected String title;
+	protected String artist;
+	protected double price;
+	protected String licenseType;
 
-    private String licensingTerms;     // Encapsulation
-    private String selectedLicense;    // User selected license
+	// Encapsulation
+	private String licensingTerms;
+	private String selectedLicense;
 
-    // Constructor without preview
-    public Artwork(String title, String artist, double price, String licenseType) {
-        this.title = title;
-        this.artist = artist;
-        this.price = price;
-        this.licenseType = licenseType;
-        this.licensingTerms = "Standard Usage";
-    }
+	// Constructor without preview
+	public Artwork(String title, String artist, double price, String licenseType) {
+		this.title = title;
+		this.artist = artist;
+		this.price = price;
+		this.licenseType = licenseType;
+		this.licensingTerms = "Standard Usage";
+	}
 
-    // Setter & Getter for selected license
-    public void setSelectedLicense(String selectedLicense) {
-        this.selectedLicense = selectedLicense;
-    }
+	// Setter & Getter for selected license
+	public void setSelectedLicense(String selectedLicense) {
+		this.selectedLicense = selectedLicense;
+	}
 
-    protected String getSelectedLicense() {
-        return selectedLicense;
-    }
+	protected String getSelectedLicense() {
+		return selectedLicense;
+	}
 
-    protected String getLicensingTerms() {
-        return licensingTerms;
-    }
+	protected String getLicensingTerms() {
+		return licensingTerms;
+	}
 
-    protected void setLicensingTerms(String terms) {
-        this.licensingTerms = terms;
-    }
-    
-    public double getPrice() {
-        return price;
-    }
+	protected void setLicensingTerms(String terms) {
+		this.licensingTerms = terms;
+	}
 
-    public abstract void displayDetails();
+	public double getPrice() {
+		return price;
+	}
+
+	//abstract method (need to be override by sub classes)
+	public abstract void displayDetails();
 }
