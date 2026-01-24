b08d46dbff05725439b22544bd21f40967c4ea33
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-01-24 23:43:04 +0530

    [Anjali] : Added program for validate credit card

---
+++ java-collections-practice/gcr-codebase/CollectionsPractice/src/com/regex/validatecreditcard/CreditCardNumber.java
@@ -0,0 +1,40 @@
+package com.regex.validatecreditcard;
+
+import java.util.Scanner;
+import java.util.regex.*;
+public class CreditCardNumber {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		//taking user input for visa number
+		System.out.print("Enter Visa number : ");
+		String visa = input.nextLine();
+		String regex = "^[4][0-9]{15}$";
+		Pattern pattern1 = Pattern.compile(regex);
+		
+		Matcher matcher = pattern1.matcher(visa);
+		if(matcher.matches()) {
+			System.out.println("Visa number is Valid");
+		}
+		else {
+			System.out.println("Invalid visa number");
+		}
+		
+		//taking user input for master card number
+		System.out.print("Enter MasterCard number : ");
+		String masterCard = input.nextLine();
+		
+		String regex2 = "^[5][0-9]{15}$";
+		Pattern pattern2 = Pattern.compile(regex2);
+		
+		Matcher matcher2 = pattern2.matcher(regex2);
+		if(matcher.matches()) {
+			System.out.println("MasterCard number is Valid");
+		}
+		else {
+			System.out.println("Invalid MasterCard number");
+		}
+		
+	}
+
+}
