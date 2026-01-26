7d698471748be31e571fb993783c00fa7cacb4ef
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-01-26 13:10:42 +0530

    [Anjali] : Added program for testing file handling

---
+++ java-collections-practice/gcr-codebase/CollectionsPractice/src/com/junit/filehandlingtest/FileProcessor.java
@@ -0,0 +1,28 @@
+package com.junit.filehandlingtest;
+import java.io.*;
+public class FileProcessor {
+	
+	public void writeToFile(String filename, String content) throws IOException {
+		// FileWriter is used to write character data to a file
+        FileWriter writer = new FileWriter(filename);
+        writer.write(content); // write content
+        writer.close(); 
+		
+	}
+
+	public String readFromFile(String filename) throws IOException {
+	 BufferedReader reader = new BufferedReader(new FileReader(filename));
+     StringBuilder content = new StringBuilder();
+     String line;
+
+     // Read file line by line
+     while ((line = reader.readLine()) != null) {
+         content.append(line);
+     }
+
+     reader.close();
+     return content.toString();
+		
+	}
+
+}
---
+++ java-collections-practice/gcr-codebase/CollectionsPractice/src/com/junit/filehandlingtest/TestFileProcessor.java
@@ -0,0 +1,38 @@
+package com.junit.filehandlingtest;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.io.IOException;
+public class TestFileProcessor {
+	FileProcessor processor = new FileProcessor();
+    String filename = "testfile.txt";
+    
+    @Test
+    void testWriteAndReadFile() throws IOException{
+    	String data = "Hello I am Anjali";
+    	processor.writeToFile(filename, data);       // write data into a file
+        String result = processor.readFromFile(filename); // read data
+
+        assertEquals(data, result);
+    }
+    
+    //File exists after writing
+    @Test
+    void testFileExistsAfterWrite() throws IOException{
+    	processor.writeToFile(filename, "File Exists Test");
+
+        File file = new File(filename);
+        assertTrue(file.exists(), "File should exist after writing");
+    }
+    
+    //IOException when file does not exist
+    @Test
+    void testReadNonExistingFile() {
+        assertThrows(IOException.class, () -> {
+            processor.readFromFile("nofile.txt");
+        });
+    }
+
+}
