4dacb36fc4291eb3e0ff03c037e798782dcd023d
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-01-26 13:21:53 +0530

    [Anjali] : Added testing program for valid password

---
+++ java-collections-practice/gcr-codebase/CollectionsPractice/src/com/junit/passwordstrength/PasswordValidator.java
@@ -0,0 +1,21 @@
+package com.junit.passwordstrength;
+
+public class PasswordValidator {
+	private static final String PASSWORD_REGEX =
+            "^(?=.*[A-Z])(?=.*\\d).{8,}$";
+	
+	public boolean isValid(String password) {
+
+        if (password == null) {
+            throw new IllegalArgumentException("Password cannot be null");
+        }
+
+        // matches() checks the regex
+        if (!password.matches(PASSWORD_REGEX)) {
+            throw new IllegalArgumentException("Weak password");
+        }
+
+        return true;
+    }
+
+}
---
+++ java-collections-practice/gcr-codebase/CollectionsPractice/src/com/junit/passwordstrength/TestPasswordValidator.java
@@ -0,0 +1,35 @@
+package com.junit.passwordstrength;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestPasswordValidator {
+	PasswordValidator validator = new PasswordValidator();
+
+	@Test
+	void testValidPassword() {
+		assertTrue(validator.isValid("StrongPass1"));
+	}
+
+	@Test
+	void testNoUppercase() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			validator.isValid("password1");
+		});
+	}
+
+	@Test
+	void testNoDigit() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			validator.isValid("Password");
+		});
+	}
+
+	@Test
+	void testShortPassword() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			validator.isValid("Abc1");
+		});
+	}
+
+}
