500c45a944b9a41a242314f4fa9041deffe12422
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-01-26 13:19:02 +0530

    [Anjali] : Added testing program for parameterized tests

---
+++ java-collections-practice/gcr-codebase/CollectionsPractice/src/com/junit/testparametrizedtests/Parametrized.java
@@ -0,0 +1,13 @@
+package com.junit.testparametrizedtests;
+
+public class Parametrized {
+	
+	public boolean isEven(int number) {
+		if(number < 0) {
+			throw new IllegalArgumentException("Negative numbers not allowed : " + number);
+		}
+		
+		return number%2 == 0;
+	}
+
+}
---
+++ java-collections-practice/gcr-codebase/CollectionsPractice/src/com/junit/testparametrizedtests/ParametrizedTest.java
@@ -0,0 +1,30 @@
+package com.junit.testparametrizedtests;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.*;
+public class ParametrizedTest {
+	Parametrized para = new Parametrized();
+	
+	//this test will run six times with the different given values
+	@ParameterizedTest
+	
+	//this will provide multiple input values to a single methos
+	@ValueSource(ints = {2,4,6,7,9,-5})
+	void testIsEven(int number) {
+		try {
+			boolean result = para.isEven(number);
+			
+			if(number % 2 == 0) {
+				assertTrue(result, number + "should be even");
+			}else {
+				assertFalse(result, number + "should be odd");
+			}
+		} catch(IllegalArgumentException e) {
+			
+			//handle negative number
+			System.out.println("Exception caught : " + e.getMessage());
+		}
+	}
+
+}
