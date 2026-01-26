4a2c183056f20df2a38c37ae433191806015d5d6
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-01-26 13:15:20 +0530

    [Anjali] : Added testing program for calculator operations

---
+++ java-collections-practice/gcr-codebase/CollectionsPractice/src/com/junit/calculatortest/CalculatorTest.java
@@ -0,0 +1,36 @@
+package com.junit.calculatortest;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class CalculatorTest {
+	Calculator cal;
+	@BeforeEach
+	void setup() {
+		cal = new Calculator();
+	}
+	@Test
+	void testAddition() {
+		assertEquals(10,cal.add(5,5), "Addition should be correct");
+	}
+	@Test
+	void testSubtraction() {
+		assertEquals(2,cal.subtract(5,3), "Subtraction should be correct");
+	}
+	@Test
+	void testMultiplication() {
+		assertEquals(15,cal.multiply(5,3), "Multiplication should be correct");
+	}
+	@Test
+	void testDivision() {
+		assertEquals(2,cal.divide(10,5), "Division should be correct");
+	}
+	
+	@Test
+	void testDivisionByZero() {
+		assertThrows(ArithmeticException.class, () -> cal.divide(10,0));
+	}
+}
