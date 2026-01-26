aaf5cb3f618c032487086942dbcc0fc260152af5
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-01-26 13:27:48 +0530

    [Anjali] : Added testing program for list operations

---
+++ java-collections-practice/gcr-codebase/CollectionsPractice/src/com/junit/testlistoperations/ListManager.java
@@ -0,0 +1,29 @@
+package com.junit.testlistoperations;
+
+import java.util.List;
+
+public class ListManager {
+	public void addElement(List<Integer> list, int element) {
+		if(list == null) {
+			throw new IllegalArgumentException("List cannot be null");
+		}
+		list.add(element);
+	}
+	public void removeElement(List<Integer> list, int element) {
+		if(list == null) {
+			throw new IllegalArgumentException("List cannot be null");
+		}
+		if (!list.contains(element)) {
+            throw new IllegalArgumentException("Element not found in list");
+        }
+		//remove particular element
+		list.remove(Integer.valueOf(element));
+	}
+	public int getSize(List<Integer> list) {
+		if (list == null) {
+            throw new IllegalArgumentException("List cannot be null");
+        }
+		return list.size();
+	}
+
+}
---
+++ java-collections-practice/gcr-codebase/CollectionsPractice/src/com/junit/testlistoperations/ListManagerTest.java
@@ -0,0 +1,65 @@
+package com.junit.testlistoperations;
+
+import java.util.List;
+import java.util.ArrayList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class ListManagerTest {
+	ListManager manager;
+	List<Integer> list;
+	
+	@BeforeEach
+	void setup() {
+		manager = new ListManager();
+		list = new ArrayList<>();
+	}
+	
+	@Test
+	void testAddElement() {
+		manager.addElement(list, 10);
+		manager.addElement(list, 20);
+		
+		assertEquals(2, manager.getSize(list));
+		assertTrue(list.contains(10));
+		assertTrue(list.contains(20));
+	}
+
+	@Test
+	void testRemoveElement() {
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		manager.removeElement(list, 20);
+		
+		assertEquals(2, manager.getSize(list));
+		assertFalse(list.contains(20));
+	}
+	
+	@Test
+	void testGetSize() {
+		assertEquals(0, manager.getSize(list));
+
+        manager.addElement(list, 5);
+        manager.addElement(list, 15);
+
+        assertEquals(2, manager.getSize(list));
+	}
+	
+	@Test
+    void testRemoveElementNotFound() {
+        Exception ex = assertThrows(
+            IllegalArgumentException.class,
+            () -> manager.removeElement(list, 50)
+        );
+        assertEquals("Element not found in list", ex.getMessage());
+    }
+
+    @Test
+    void testNullList() {
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> manager.addElement(null, 10)
+        );
+    }
+}
